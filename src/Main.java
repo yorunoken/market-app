7fe10315970a4b6b38d8702799c82a59c9adf632
yorunoken <me@yorunoken.com>
2025-12-24 13:22:01 +0300

    Refactor Main class and update main method

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,19 @@ import com.sun.net.httpserver.HttpServer;
 import handlers.ApiRouter;
 import handlers.StaticFileHandler;
 
-private static final int PORT = 8000;
+import java.io.IOException;
+import java.net.InetSocketAddress;
 
-void main() throws IOException {
-    HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
-    server.createContext("/api", new ApiRouter());
-    server.createContext("/", new StaticFileHandler());
+public class Main {
+    private static final int PORT = 8000;
 
-    server.setExecutor(null);
-    IO.println("Server started at http://localhost:" + PORT);
-    server.start();
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
+        server.createContext("/api", new ApiRouter());
+        server.createContext("/", new StaticFileHandler());
+
+        server.setExecutor(null);
+        System.out.println("Server started at http://localhost:" + PORT);
+        server.start();
+    }
 }
